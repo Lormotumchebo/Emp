50accedaeb1ee6b2df2ae6fcd64dc662f1cb336c
Noel <noelstrath5@gmail.com>
2025-02-10 15:06:52 +0300

    new

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Far.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Far.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
